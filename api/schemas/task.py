dbe87dd9512d65f44672d45a2eacfffdc6788641
tsutsumi-ozro <soccerbakadaiki@icloud.com>
2023-11-18 17:34:00 +0900

    done 10

--- api/schemas/task.py
+++ api/schemas/task.py
@@ -3,7 +3,25 @@ from typing import Optional
 from pydantic import BaseModel, Field
 
 
-class Task(BaseModel):
+class TaskBase(BaseModel):
+    title : Optional[str] = Field(None, example="クリーニングを取りに行く")
+
+
+class TaskCreate(TaskBase):
+    pass
+
+
+class TaskCreateResponse(TaskCreate):
     id: int
-    title: Optional[str] = Field(None, example="クリーニングを取りに行く")
+
+    class Config:
+        orm_mode = True
+
+
+class Task(TaskBase):
+    id: int
     done: bool = Field(False, description="完了フラグ")
+
+    class Config:
+        orm_mode = True
+
